9baced357f1dd5a509404748f3c99d9a2688c9f9
Jia Lin <v-jilin2@microsoft.com>
2019-06-19 19:36:03 +0800

    24

--- Electron-react/travelplan/src/pages/Welcome/App.js
+++ Electron-react/travelplan/src/pages/Welcome/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../assets/logo.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.123
+          Edit <code>src/App.js</code> and save to reload. Welcome
         </p>
         <a
           className="App-link"
